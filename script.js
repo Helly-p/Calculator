e7ca1c09c1e13fd6b9a4313b56b8c1b52a62c7c7
ihellybprajapati829 <ihellybprajapati829@gmail.com>
2023-03-12 00:40:52 +0530

    Hyp & Inverse Trigo functions added.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const values = {
     'π': 3.14,
     'e': 2.718,
     '^2': '**2',
+    '^3': '**3',
     ' mod ': '%',
     ' exp ': '2.718**',
     '^': '**',
@@ -10,16 +11,31 @@ const values = {
     'floor(': 'Math.floor(',
     'ceil(': 'Math.ceil(',
     '10^': '10**',
+    '2^': '2**',
     'sqrt(': 'Math.sqrt(',
+    'cbrt(': 'Math.cbrt(',
     'log(': 'Math.log10(',
     'ln(': 'Math.log(',
+    'log2(': 'Math.log2(',
     'sin(': 'Math.sin(',
     'cos(': 'Math.cos(',
     'tan(': 'Math.tan(',
     'cosec(': '1/Math.sin(',
     'sec(': '1/Math.cos(',
     'cot(': '1/Math.tan(',
-    'fact(': 'factorial('
+    'fact(': 'factorial(',
+    'asin(': 'Math.asin(',
+    'acosec(': '1/Math.asin(',
+    'acos(': 'Math.acos(',
+    'asec(': '1/Math.acos(',
+    'atan(': 'Math.atan(',
+    'acot(': '1/Math.atan(',
+    'sinh(': 'Math.sinh(',
+    'cosech(': '1/Math.sinh(',
+    'cosh(': 'Math.cosh(',
+    'sech(': '1/Math.cosh(',
+    'tanh(': 'Math.tanh(',
+    'coth(': '1/Math.tanh(',
 }
 var result = '';
 var output;
@@ -30,46 +46,14 @@ function factorial(data) {
     }
     return fact;
 }
-function answer(text) {
-    let pattern = /^[0-9 \+ \- \* \/\(\) \. \^]+$/;
-
-    if (!text.match(pattern)) {
-        document.getElementById("error").innerHTML = "*Enter valid characters.";
-    }
-    else {
-        document.getElementById("error").innerHTML = "";
-    }
-}
 function putdata(data) {
-    var cursor_pos = input.selectionStart;
-    var cursor;
-    console.log("before " + cursor_pos);
-    input.value = input.value.substring(0, cursor_pos) + data + input.value.substring(cursor_pos);
-    console.log("After " + cursor_pos);
-
-    input.selectionStart = cursor_pos + data.length;
-    input.focus();
-    // input.selectionEnd = cursor_pos + data.length;
-    // console.log(data.length);
-    // input.value += data;
-    if (data == "fact(") {
-        // alert("Hello");
-        input.value += ')';
-        cursor = input.selectionStart - 1;
-        console.log(cursor)
-        input.setSelectionRange(cursor, cursor);
-        input.focus();
-    }
-    // alert(input.selectionEnd)
+    input.value += data;
     for (let x in values) {
         if (x == data) {
             data = values[x];
         }
     }
-
     result += data;
-    // console.log("Putdata")
-    // console.log(result);
 }
 function storeinMemory() {
     console.log(result);
@@ -90,6 +74,7 @@ function deleteChar() {
 }
 
 function showAns() {
+    let count = 0, pattern1 = /[\(]/g, pattern2 = /[\)]/g;
     try {
         let op;
         if (result == 0) {
@@ -98,12 +83,19 @@ function showAns() {
         else {
             op = result;
         }
+        count = op.replace(pattern1, "").length;
+        // console.log(op.match(pattern2).length);
+        if (count != op.replace(pattern2, "").length) {
+            // alert("Hello")
+            throw "*Put valid paranthesis.";
+        }
         output = eval(op);
         document.getElementById("ans").value = output;
+        document.getElementById("error").innerHTML = "";
     }
     catch (e) {
-        alert(e);
-        document.getElementById("error").innerHTML = "*Enter valid expression.";
+        // alert(e);
+        document.getElementById("error").innerHTML = "*Enter valid expression.<br>" + e;
     }
 }
 
@@ -130,5 +122,62 @@ function storeinMemory(value) {
         data = localStorage.getItem('calcdata');
         exp = input.value + '-' + data;
         input.value = eval(exp);
+    }
+}
+function second_menu() {
+    let first_class = document.getElementsByClassName("first");
+    let second_class = document.getElementsByClassName("second");
+    let style = getComputedStyle(document.querySelector(".first")).display;
+    if (style == "inline-block") {
+        for (let i = 0; i < 6; i++) {
+            first_class[i].style.display = "none";
+            second_class[i].style.display = "inline-block";
+        }
+    }
+    else {
+        for (let i = 0; i < 6; i++) {
+            first_class[i].style.display = "inline-block";
+            second_class[i].style.display = "none";
+        }
+    }
+}
+function inverse_menu() {
+    let basic = document.getElementsByClassName("basic");
+    let inverse = document.getElementsByClassName("inverse");
+    let hyp = document.getElementsByClassName("hyp");
+    let basic_style = getComputedStyle(document.querySelector(".basic")).display;
+    let hyp_style = getComputedStyle(document.querySelector(".hyp")).display;
+    if (basic_style == "inline-block" || hyp_style == "inline-block") {
+        for (let i = 0; i < 6; i++) {
+            basic[i].style.display = "none";
+            hyp[i].style.display = "none";
+            inverse[i].style.display = "inline-block";
+        }
+    }
+    else {
+        for (let i = 0; i < 6; i++) {
+            basic[i].style.display = "inline-block";
+            inverse[i].style.display = "none";
+        }
+    }
+}
+function hyp_menu() {
+    let basic = document.getElementsByClassName("basic");
+    let inverse = document.getElementsByClassName("inverse");
+    let hyp = document.getElementsByClassName("hyp");
+    let basic_style = getComputedStyle(document.querySelector(".basic")).display;
+    let inverse_style = getComputedStyle(document.querySelector(".inverse")).display;
+    if (basic_style == "inline-block" || inverse_style == "inline-block") {
+        for (let i = 0; i < 6; i++) {
+            basic[i].style.display = "none";
+            inverse[i].style.display = "none";
+            hyp[i].style.display = "inline-block";
+        }
+    }
+    else {
+        for (let i = 0; i < 6; i++) {
+            basic[i].style.display = "inline-block";
+            hyp[i].style.display = "none";
+        }
     }
 }
